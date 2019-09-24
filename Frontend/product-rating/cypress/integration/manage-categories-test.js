ae9fd17557a4347a40d8bfa7064243ecaed69b80
Kulacs Péter <petya255@gmail.com>
2019-09-24 14:35:06 +0200

    manage-category tests

---
+++ Frontend/product-rating/cypress/integration/manage-categories-test.js
@@ -0,0 +1,55 @@
+describe("Manage categoires test", () => {
+    it("should visit category list", () => {
+        cy.visit("/manage-categories");
+
+        cy.get('.ngx-datatable').find('.datatable-body-row').its('length').should('be.gt', 0);
+        cy.get('.ngx-datatable').should('contain', "Name");
+        cy.get('.ngx-datatable').should('contain', "Num Of Products");
+        cy.get('.ngx-datatable').should('contain', "Attribute Names");
+        cy.get('.ngx-datatable').should('contain', "Parent Name");
+    });
+
+    it("should add a new category", () => {
+        cy.visit("/manage-categories/new");
+
+        //type category name
+        cy.get('input[name="category-name"]')
+            .clear()
+            .type('Digital camera');
+
+        //add an attribute
+        cy.get('a').contains('New attribute').click();
+        cy.wait(200);
+
+        cy.get('input[name="name"]')
+            .clear()
+            .type('Max ISO');
+
+        cy.get('.attribute-item select')
+            .select('2');
+
+        //save category 
+        cy.get('button').contains('Save Category').click();
+
+        //check 
+        cy.get('.ngx-datatable').should('contain', "Digital camera");
+    });
+
+    it("should delete category", () => {
+
+        cy.get('input[name="name"]')
+            .clear()
+            .type('Camera');
+        cy.get('button').contains('Filter').click();
+
+        cy.get('a').contains('Delete').first().click();
+
+        cy.wait(200);
+        cy.get('button').contains('Ok').click();
+        cy.wait(200);
+
+        //check     
+        cy.get('.ngx-datatable').should('not.contain', 'Digital camera');
+
+    });
+});
